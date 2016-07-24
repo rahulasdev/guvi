e22d0f83234cede7fe35905126d368127b99f0d9
rahulasdev <rahulasdev@users.noreply.github.com>
2016-07-25 01:42:01 +0530

    Add files via upload

---
+++ sumodd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int i;
+	int sum=1;
+	int f=1;
+	scanf("%d",&n);
+	for(i=3;i<=9;i=i+2){
+		if(f==1){
+			f=0;
+			sum=sum+i;
+		}
+		else{
+			f=1;
+			sum=sum-i;
+		}	
+	}
+	printf("%d",sum);
+}
