bcea769643f8f560010042f52bf6c5d9e4556012
rahulasdev <rahulasdev@users.noreply.github.com>
2016-07-25 01:18:56 +0530

    Add files via upload

---
+++ SameString.java
@@ -0,0 +1,21 @@
+package maxlength;
+
+import java.util.Scanner;
+
+public class SameString {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String input1=s.nextLine();
+		String input2=s.next();
+		String str[]=input1.split(" ");
+		for(int i=0;i<str.length;i++){
+			if(input2.equals(str[i])){
+				System.out.println(i);
+				break;
+			}
+		}
+
+	}
+
+}
